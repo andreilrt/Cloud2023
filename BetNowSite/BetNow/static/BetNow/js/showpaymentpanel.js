54b48843bd5295ea19aca7e7f818fa3cde933609
andreilrt <andreiriano@usantotomas.edu.co>
2023-04-18 12:31:13 -0500

    cambios en js

--- BetNowSite/BetNow/static/BetNow/js/showpaymentpanel.js
+++ BetNowSite/BetNow/static/BetNow/js/showpaymentpanel.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+
+    //<!-- CSRF Setup Script -->
+    // Add this code to make the CSRF token available for AJAX requests
+    var csrftoken = '{{ csrf_token }}';
+
+    function csrfSafeMethod(method) {
+        // These HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
     // Function for handling payment-method change
     $("#payment-method").on("change", function () {
         var selectedPaymentMethod = $(this).val();
@@ -47,5 +65,15 @@ $(document).ready(function () {
             $(".invalid-feedback").empty();
         }
     });
-});
 
+    const passwordInput = document.querySelector('#password');
+    const showPasswordCheckbox = document.querySelector('#show-password');
+
+    showPasswordCheckbox.addEventListener('click', function() {
+        if (showPasswordCheckbox.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+});
