b179862a88ec0e3c3e0857b84e476d1c682af303
andreilrt <andreiriano@usantotomas.edu.co>
2023-04-19 17:47:46 -0500

    Probando el uso del modelo, sin exito aún

--- BetNowSite/BetNow/models.py
+++ BetNowSite/BetNow/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Perfil(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
--- BetNowSite/BetNow/views.py
+++ BetNowSite/BetNow/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Perfil
 
 # Create your views here.
 def index(request):
@@ -20,6 +21,26 @@ def Registro(request):
     return render(request,"BetNow/signup.html", context)
 
 def usuario_inicio(request):
+    if request.method == 'POST':
+        # Extract the values of the form fields from the request.POST dictionary
+        nombres = request.POST['first-name']
+        apellidos = request.POST['last-name']
+        password = request.POST['password']
+        pais = request.POST['country']
+        ciudad = request.POST['city']
+        direccion = request.POST['address']
+        email = request.POST['email']
+        indicativo = request.POST['phone-prefix']
+        celular = request.POST['phone']
+        documento = request.POST['document-type']
+        numero_documento = request.POST['document-number']
+        fecha_expedicion = request.POST['issue-date']
+
+        # Create a new Perfil object with the extracted values
+        perfil = Perfil.objects.create(nombres=nombres, apellidos=apellidos, password=password, pais=pais, ciudad=ciudad, direccion=direccion, email=email, indicativo=indicativo, celular=celular, documento=documento, numero_documento=numero_documento, fecha_expedicion=fecha_expedicion)
+
+        # Redirect to the success page
+        return redirect('BetNow/usuario_inicio.html')
     context = {
         'Bet_Inicio': '/static/BetNow/img/Bet_Inicio.svg',
         'Basketball': '/static/BetNow/img/Basketball.svg',
@@ -29,4 +50,4 @@ def usuario_inicio(request):
         'Avatar': '/static/BetNow/img/Avatar.svg',
         'logo': '/static/BetNow/img/logo.svg',
     }
-    return render(request, "BetNow/usuario_inicio.html", context)
+    return render(request, "BetNow/Registro.html", context)
