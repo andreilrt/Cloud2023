85e65942199de67eeab31732afbec758bbaf380c
andreilrt <andreilizandroriano@gmail.com>
2023-06-07 14:18:29 -0500

    cambio en los modelos

--- BetNowSite/BetNow/models.py
+++ BetNowSite/BetNow/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     pais = models.CharField(max_length=100)
     ciudad = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
@@ -13,3 +15,40 @@ class Perfil(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Banco(models.Model):
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    nombre_titular = models.CharField(max_length=100)
+    numero_cuenta = models.CharField(max_length=20)
+    tipo_cuenta = models.CharField(max_length=20)
+    nombre_banco = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_banco
+
+
+class PSE(models.Model):
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    banco = models.CharField(max_length=100)
+    tipo_cuenta = models.CharField(max_length=50)
+    tipo_documento = models.CharField(max_length=50)
+    numero_documento = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo_electronico = models.EmailField()
+
+    def __str__(self):
+        return self.banco
+
+
+class Tarjeta(models.Model):
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    nombre_titular = models.CharField(max_length=100)
+    numero_tarjeta = models.CharField(max_length=16)
+    fecha_expiracion = models.DateField()
+    cvv = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.nombre_titular
+
--- BetNowSite/BetNow/views.py
+++ BetNowSite/BetNow/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, redirect
-from .models import Perfil
+from .models import Perfil, Banco, PSE, Tarjeta
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -59,9 +59,49 @@ def Registro(request):
 
         # Create a new user object with the extracted values
         user = User.objects.create_user(username=email, password=password, first_name=nombres, last_name=apellidos, email=email)
-        new_user = Perfil.objects.create(pais=pais, ciudad=ciudad, direccion=direccion, email=email, indicativo=indicativo, celular=celular, documento=documento, numero_documento=numero_documento, fecha_expedicion=fecha_expedicion)
+
+        # Associate the user with the perfil
+        perfil = Perfil.objects.create(user=user, pais=pais, ciudad=ciudad, direccion=direccion, email=email, indicativo=indicativo, celular=celular, documento=documento, numero_documento=numero_documento, fecha_expedicion=fecha_expedicion)
+        
         user.save()
-        new_user.save()
+        perfil.save()
+
+        # Check the selected payment method and save the corresponding information
+        payment_method = request.POST.get('payment-method')
+
+        if payment_method == 'banks':
+            if 'colombian-banks' in request.POST:
+                # Process bank information
+                banco_nombre = request.POST['colombian-banks']
+                titular = request.POST['account-name']
+                numero_cuenta = request.POST['account-number']
+                tipo_cuenta = request.POST['account-type']
+                banco = Banco.objects.create(perfil=perfil, nombre_titular=titular, numero_cuenta=numero_cuenta, tipo_cuenta=tipo_cuenta, nombre_banco=banco_nombre)
+                banco.save()
+
+        elif payment_method == 'pse':
+            if 'pse-bank' in request.POST:
+                # Process PSE information
+                banco = request.POST['pse-bank']
+                tipo_cuenta = request.POST['pse-account-type']
+                tipo_documento = request.POST['pse-document-type']
+                numero_documento = request.POST['pse-document-number']
+                nombre = request.POST['pse-first-name']
+                apellido = request.POST['pse-last-name']
+                correo_electronico = request.POST['pse-email']
+                pse = PSE.objects.create(perfil=perfil, banco=banco, tipo_cuenta=tipo_cuenta, tipo_documento=tipo_documento, numero_documento=numero_documento, nombre=nombre, apellido=apellido, correo_electronico=correo_electronico)
+                pse.save()
+
+        elif payment_method == 'card':
+            if 'cardholder-name' in request.POST:
+                # Process card information
+                nombre_titular = request.POST['cardholder-name']
+                numero_tarjeta = request.POST['card-number']
+                fecha_expiracion = request.POST['month'] + '/' + request.POST['year']
+                cvv = request.POST['cvv']
+                tarjeta = Tarjeta.objects.create(perfil=perfil, nombre_titular=nombre_titular, numero_tarjeta=numero_tarjeta, fecha_expiracion=fecha_expiracion, cvv=cvv)
+                tarjeta.save()
+
         context = {
             'logo': '/static/BetNow/img/logo.svg',
             'Bet_Inicio': '/static/BetNow/img/Bet_Inicio.svg',
@@ -71,12 +111,13 @@ def Registro(request):
             'Tennis': '/static/BetNow/img/Tennis.svg',
             'Avatar': '/static/BetNow/img/Avatar.svg'
         }
+
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('inicio')
         else:
-            error_message = "Email o contraseña invalidos, por favor intente de nuevo."
+            error_message = "Email o contraseña inválidos, por favor intente de nuevo."
             context = {
                 'error_message': error_message,
                 'logo': '/static/BetNow/img/logo.svg',
@@ -92,6 +133,7 @@ def Registro(request):
             'logo': '/static/BetNow/img/logo.svg',
         }
         return render(request,"BetNow/signup.html", context)
+
 
 @login_required
 def index_usuario(request):
